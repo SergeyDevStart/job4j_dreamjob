42fdef297807688ee5f13f8fe57798e3ba454055
SergeyDevStart <vasenev.front@gmail.com>
2024-07-09 04:49:06 +0700

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
     private int nextId = 1;
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
@@ -18,10 +17,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
         save(new Candidate(0, "Sergey", "I am Intern Java Developer"));
         save(new Candidate(0, "Alex", "I am Middle Java Developer"));
         save(new Candidate(0, "Petr", "I am Senior Java Developer"));
-    }
-
-    public static MemoryCandidateRepository getInstance() {
-        return INSTANCE;
     }
 
     @Override
